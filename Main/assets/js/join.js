f8cdb74d3db32e071d1b0173caa9745bf8750514
Hardy0070 <hwv1219@gmail.com>
2024-07-24 17:46:54 +0900

    login.html, login.css / join.html, join.css, join.js 파일 수정 및 사진 추가

--- Main/assets/js/join.js
+++ Main/assets/js/join.js
@@ -1,9 +1,38 @@
+
+// 비밀번호 숨김 및 표시
+const eyeBtn = document.querySelector('#eyeBtn');
+const pwInput = document.querySelector('#pw1');
+const backImage = document.querySelector('.content');
+const boxShadow = document.querySelector('.form-list');
+// box-shadow: 0px 5px 5px 0px; 
+
+let eyeKey = false;
+
+eyeBtn.addEventListener('click', (e) => {
+    if (!eyeKey) {
+        pwInput.type = 'text';
+        eyeBtn.style.backgroundImage = 'url("/Main/assets/images/join/비밀번호표시.png")';
+        backImage.style.backgroundImage = 'url("/Main/assets/images/loginBackground2.png")';
+        boxShadow.style.boxShadow = '0px 0px 0px 0px';
+
+        eyeKey = true;
+        
+    } else {
+        pwInput.type = 'password';
+        eyeBtn.style.backgroundImage = "url('/Main/assets/images/join/비밀번호숨김.png')";
+        backImage.style.backgroundImage = 'url("/Main/assets/images/loginBackground.png")';
+        boxShadow.style.boxShadow = '0px 5px 5px 0px';
+        eyeKey = false;
+    }
+});
+
+//--------//
+// 정규식 //
+//--------//
 const idPattern = /^[a-z0-9]{5,20}$/;
-// const pwPattern = /^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{8,16}$/;
 const pwPattern = /^(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/;
 const namePattern = /^[가-힣a-zA-Z]{2,20}$/;
 const birthdayPattern = /^(19[0-9][0-9]|20[0-9][0-9])(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])$/;
-
 
 // input태그
 const idInput = document.getElementById('id');
@@ -46,11 +75,11 @@ function idBlur() {
     const inputValue = this.value;
 
 
-    // 아이디중복체크 // 아래 && 뒤에도 수정해야함.
+    // 아이디중복체크 // 수정할 때 아래 && 뒤에도 수정해야함.
     if (idInput.value === 'bitcamp') {
         divMsg1.style.visibility = 'visible';
-        idDiv.style.border = '2px solid red';
-        idInput.style.color = 'red';
+        idDiv.style.border = '2px solid #fbc70e';
+        idInput.style.color = '#fbc70e';
         idInput.placeholder = '아이디는 필수 정보입니다.';
         idInput.classList.add('errorPlaceholder');
         liMsg1.textContent = '사용할 수 없는 아이디입니다.';
@@ -58,12 +87,12 @@ function idBlur() {
     }
     // 공백과 정규식에 맞지 않는 값 처리
     if (idInput.value == '' || !idPattern.test(inputValue)) {
-        idDiv.style.border = '2px solid red';
-        idInput.style.color = 'red';
+        idDiv.style.border = '2px solid #fbc70e';
+        idInput.style.color = '#fbc70e';
         idInput.placeholder = '아이디는 필수 정보입니다.';
         idInput.classList.add('errorPlaceholder');
         divMsg1.style.visibility = 'visible';
-        liMsg1.textContent = '5~20자의 영문 소문자, 숫자만 사용 가능합니다.';
+        liMsg1.textContent = '아이디는5~20자의 영문 소문자, 숫자만 사용 가능합니다.';
     }
 
     // 맞을 때
@@ -76,7 +105,6 @@ function idBlur() {
     }
 }
 
-
 // --------------- //
 // 패스워드 메소드  //
 // --------------- //
@@ -88,47 +116,49 @@ function pw1Click() {
     pw1Div.style.border = '2px solid #ddd';
     pw1Input.style.color = '#fff';
     pw1Input.classList.remove('errorPlaceholder');
-    pw1Input.placeholder = '8~16자의 영문 대/소문자, 숫자, 특수문자를 사용해 주세요.';
+    pw1Input.placeholder = '8~16자의 영문, 숫자, 특수문자를 사용해 주세요.';
 }
 
 function pw1Blur() {
     const inputValue = this.value;
 
-    // 공백과 정규식에 맞지 않는 값 처리
-    // if (pwPattern.test(inputValue)) {
-        if (pw1Input.value == '' || !pwPattern.test(inputValue)) {
-            pw1Div.style.border = '2px solid red';
-            pw1Input.style.color = 'red';
-            pw1Input.placeholder = '비밀번호는 필수 정보입니다.';
-            pw1Input.classList.add('errorPlaceholder');
+    if (pw1Input.value == '' || !pwPattern.test(inputValue)) {
+        pw1Div.style.border = '2px solid #fbc70e';
+        pw1Input.style.color = '#fbc70e';
+        pw1Input.placeholder = '비밀번호는 필수 정보입니다.';
+        pw1Input.classList.add('errorPlaceholder');
+        divMsg2.style.visibility = 'visible';
+        liMsg2.textContent = '비밀번호는 8~16자의 영문, 숫자, 특수문자를 사용해 주세요.';
 
-            if (pw2Input.value != pw1Input.value && !pw2Input.value == '') {
-                pw2Div.style.border = '2px solid red';
-                pw2Input.style.color = 'red';
-                pw2Input.placeholder = '비밀번호를 확인해 주세요.';
-                pw2Input.classList.add('errorPlaceholder');
+        if (pw2Input.value != pw1Input.value && !pw2Input.value == '') {
+            pw2Div.style.border = '2px solid #fbc70e';
+            pw2Input.style.color = '#fbc70e';
+            pw2Input.placeholder = '비밀번호를 확인해 주세요.';
+            pw2Input.classList.add('errorPlaceholder');
 
-            } else if (pw2Input.value == pw1Input.value && !pw1Input.value == '' && pwPattern.test(inputValue)) {
-                pw2Div.style.border = '0px';
-                pw2Input.classList.remove('errorPlaceholder');
-                pw2Input.style.color = '#fff';
-            }
+        } else if (pw2Input.value == pw1Input.value && !pw1Input.value == ''
+            && pwPattern.test(inputValue)) {
+            pw2Div.style.border = '0px';
+            pw2Input.classList.remove('errorPlaceholder');
+            pw2Input.style.color = '#fff';
+            divMsg2.style.visibility = 'hidden';
+            liMsg2.textContent = '사용할 수 없는 닉네임입니다.';
         }
+    }
 
-        if (pwPattern.test(inputValue) || (pw2Input.value == pw1Input.value
-            && !pw1Input.value == '')) {
-            pw1Div.style.border = '0px';
-            pw1Input.classList.remove('errorPlaceholder');
-            pw1Input.style.color = '#fff';
+    if (pwPattern.test(inputValue) || (pw2Input.value == pw1Input.value
+        && !pw1Input.value == '' && pwPattern.test(inputValue))) {
+        pw1Div.style.border = '0px';
+        pw1Input.classList.remove('errorPlaceholder');
+        pw1Input.style.color = '#fff';
 
-            if (pw2Input.value != pw1Input.value && !pw2Input.value == '') {
-                pw2Div.style.border = '2px solid red';
-                pw2Input.style.color = 'red';
-                pw2Input.placeholder = '비밀번호를 확인해 주세요.';
-                pw2Input.classList.add('errorPlaceholder');
-            }
+        if (pw2Input.value != pw1Input.value && !pw2Input.value == '') {
+            pw2Div.style.border = '2px solid #fbc70e';
+            pw2Input.style.color = '#fbc70e';
+            pw2Input.placeholder = '비밀번호를 확인해 주세요.';
+            pw2Input.classList.add('errorPlaceholder');
         }
-    // }
+    }
 }
 
 function pw1Check() {
@@ -158,7 +188,7 @@ function pw2Click() {
     pw2Div.style.border = '2px solid #ddd';
     pw2Input.style.color = '#fff';
     pw2Input.classList.remove('errorPlaceholder');
-    pw2Input.placeholder = '8~16자의 영문 대/소문자, 숫자, 특수문자를 사용해 주세요.';
+    pw2Input.placeholder = '8~16자의 영문, 숫자, 특수문자를 사용해 주세요.';
 }
 
 function pw2Blur() {
@@ -167,8 +197,8 @@ function pw2Blur() {
     // 공백과 정규식에 맞지 않는 값 처리
     if (pw2Input.value == '' || pw2Input.value != pw1Input.value
         || !pwPattern.test(inputValue)) {
-        pw2Div.style.border = '2px solid red';
-        pw2Input.style.color = 'red';
+        pw2Div.style.border = '2px solid #fbc70e';
+        pw2Input.style.color = '#fbc70e';
         pw2Input.placeholder = '비밀번호를 확인해 주세요.';
         pw2Input.classList.add('errorPlaceholder');
     }
@@ -177,6 +207,8 @@ function pw2Blur() {
         pw2Div.style.border = '0px';
         pw2Input.classList.remove('errorPlaceholder');
         pw2Input.style.color = '#fff';
+        divMsg2.style.visibility = 'hidden';
+        liMsg2.textContent = '사용할 수 없는 닉네임입니다.';
     }
 }
 
@@ -204,7 +236,6 @@ function nicknameClick() {
     nicknameInput.classList.remove('errorPlaceholder');
     nicknameInput.placeholder = '닉네임';
     liMsg2.textContent = '사용할 수 없는 닉네임입니다.';
-
 }
 
 function nicknameBlur() {
@@ -217,18 +248,18 @@ function nicknameBlur() {
         nicknameInput.style.color = '#fff';
         divMsg2.style.visibility = 'hidden';
 
-    // 닉네임 중복일 때
-    } else if(nicknameInput.value === '고기천') {
+        // 닉네임 중복일 때
+    } else if (nicknameInput.value === '고기천') {
         divMsg2.style.visibility = 'visible';
-        nicknameInput.style.color = 'red';
-        nicknameDiv.style.border = '2px solid red';
+        nicknameInput.style.color = '#fbc70e';
+        nicknameDiv.style.border = '2px solid #fbc70e';
         nicknameInput.placeholder = '닉네임은 필수 정보입니다.';
         nicknameInput.classList.add('errorPlaceholder');
 
 
     } else {
-        nicknameDiv.style.border = '2px solid red';
-        nicknameInput.style.color = 'red';
+        nicknameDiv.style.border = '2px solid #fbc70e';
+        nicknameInput.style.color = '#fbc70e';
         nicknameInput.placeholder = '닉네임은 필수 정보입니다.';
         nicknameInput.classList.add('errorPlaceholder');
     }
@@ -277,21 +308,13 @@ function nameClick() {
 function nameBlur() {
     const inputValue = this.value;
 
-    // 공백과 정규식에 맞지 않는 값 처리
-    // if (nicknameInput.value == '') {
-    //     nicknameDiv.style.border = '2px solid red';
-    //     nicknameInput.style.color = 'red';
-    //     nicknameInput.placeholder = '닉네임은 필수 정보입니다.';
-    //     nicknameInput.classList.add('errorPlaceholder');
-    // }
-
     if (namePattern.test(inputValue)) {
         nameDiv.style.border = '0px';
         nameInput.classList.remove('errorPlaceholder');
         nameInput.style.color = '#fff';
     } else {
-        nameDiv.style.border = '2px solid red';
-        nameInput.style.color = 'red';
+        nameDiv.style.border = '2px solid #fbc70e';
+        nameInput.style.color = '#fbc70e';
         nameInput.placeholder = '이름은 필수 정보입니다.';
         nameInput.classList.add('errorPlaceholder');
     }
@@ -318,8 +341,8 @@ function birthdayBlur() {
     if (birthdayPattern.test(inputValue)) {
         birthdayDiv.style.border = '0px';
     } else {
-        birthdayDiv.style.border = '2px solid red';
-        birthdayInput.style.color = 'red';
+        birthdayDiv.style.border = '2px solid #fbc70e';
+        birthdayInput.style.color = '#fbc70e';
         birthdayInput.placeholder = '생년월일은 8자리 숫자로 입력해 주세요.';
         birthdayInput.classList.add('errorPlaceholder');
     }
